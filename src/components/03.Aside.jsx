60ec55460917cb5b03080dc17689ea1f3988e959
Matheus <matheuseduardo.jp@gmail.com>
2022-08-19 02:56:38 -0300

    Fase-04 | Home Footer | concluida

--- src/components/03.Aside.jsx
+++ src/components/03.Aside.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Aside extends Component {
   render() {
     return (
-      <aside className='aside box'>
+      <aside className='aside box has-text-link'>
         03.Aside
         <p>
           Lorem ipsum enim eu nunc sociosqu augue elementum malesuada et risus, auctor nec morbi duis iaculis rhoncus eu fermentum at, molestie venenatis porttitor pulvinar imperdiet consectetur aptent torquent etiam. tellus velit et quisque consectetur molestie netus ad rutrum, adipiscing senectus pretium quisque enim eu fames rhoncus dolor, ligula scelerisque porttitor aliquam diam porta maecenas.
--- src/pages/01. Home.jsx
+++ src/pages/01. Home.jsx
@@ -7,17 +7,17 @@ import Footer from '../components/04.Footer';
 export default class Home extends Component {
   content = () => {
     return (
-      <div className='home-content content'>
-        <h1>Primeiramente seja Bem Vindo!</h1>
-        <h3>Me chamo Matheus Eduardo,</h3>
+      <div className='home-content content has-text-link'>
+        <h1 className='has-text-link'>Primeiramente seja Bem Vindo!</h1>
+        <h3 className='has-text-link'>Me chamo Matheus Eduardo,</h3>
         <ul>
-          <li>Sou bacharel em Ciência da Computação com MBA Gestão Empresarial;</li>
+          <li>Sou bacharelado em <strong className='has-text-link'>Ciência da Computação</strong> com <strong className='has-text-link'>MBA Gestão Empresarial</strong>;</li>
           <br/>
-          <li>Já trabalhei na área de Logística e Segurança Privada como operador de campo da transportadora multinacional Brink's, atuando em diversas funções entre elas a manutenção técnica em cofres eltrônicos e chefe de guarnição;</li>
+          <li>Atuei na área de <strong className='has-text-link'>Logística e Segurança Privada</strong> como operador de campo da transportadora multinacional Brink's, em diferentes funções tais como a manutenção técnica em cofres eletrônicos e chefe de guarnição;</li>
           <br/>
           <li>Todo este background me deu um conhecimento bastante diversificado sobre a indústria e insights poderosos para aplicação de tecnologias;</li>
           <br/>
-          <li>Hoje faço parte do corpo estudantil da Trybe e procuro desenvolver hards skills em Desenvolvimento Web para reforçar meu expertise e promover soluções;</li>
+          <li>Hoje faço parte do corpo estudantil da <strong className='has-text-link'>Trybe</strong> e procuro desenvolver hards skills em Desenvolvimento Web para reforçar meu expertise e promover soluções;</li>
         </ul>
         Rumo ao Full Stack!
         <br/>
@@ -31,6 +31,9 @@ export default class Home extends Component {
           <img alt='icon-7' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/slack/slack-original.svg" height="50" width="50"/>
           <img alt='icon-8' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/trello/trello-plain.svg" height="50" width="50"/>
           <img alt='icon-9' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" height="50" width="50"/>
+          <img alt='icon-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" height="50" width="50"/>
+          <img alt='icon-11' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" height="50" width="50"/>
+          <img alt='icon-12' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bulma/bulma-plain.svg" height="50" width="50"/>
         </div>
       </div>
     )
