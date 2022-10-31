b1916e0ac2653742bfcfd4a55b4c9f3d2f8da59a
Matheus <matheuseduardo.jp@gmail.com>
2022-10-31 18:41:46 -0300

    FIX: contact edition

--- src/components/04.Footer.jsx
+++ src/components/04.Footer.jsx
@@ -6,12 +6,12 @@ export default class Footer extends Component {
       <footer className='footer'>
         <div className='contact-content'>
           <div>
-            <a href = "https://mayarafurtado.jp@gmail.com">
+            <a href = "https://matheuseduardo.jp@gmail.com">
               <img className='contact-img' src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" alt='google' target="_blank" />
             </a>
           </div>
           <div>
-            <a href="https://www.linkedin.com/in/matheus-eduardo-b06289221" target="blank">
+            <a href="https://www.linkedin.com/in/matheuseduardosousaazevedo" target="blank">
               <img className='contact-img' src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt='linkedIn' target="_blank" />
             </a>
           </div>
